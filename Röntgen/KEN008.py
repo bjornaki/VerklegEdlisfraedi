fb4073da78da21a33a88118abacbbd1f25ca9479
bjornaki <47777021+bjornaki@users.noreply.github.com>
2020-02-17 14:38:44 +0000

    minor figure fix

--- Röntgen/KEN008.py
+++ Röntgen/KEN008.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import X_Ray_analysis as Xray
 import pdb
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
 #Import data - first XRD
 filename = "KEN008_XRD_40_85deg.xy"
@@ -29,9 +30,16 @@ DataXRR = Xray.FilmThickness(DataXRR,[0.9,4])
 
 #Plotting Data
 fig2, ax2 = Xray.PlotData(DataXRR, None, None, yaxis="log", XRR=True, Filtered=False, ThetaCr = True, showPeaks=True)
-plt.savefig("KEN008_XRR.png",format="png",dpi=300)
+ax2_inlet = inset_axes(ax2, width="50%", height=0.7, loc=1,borderpad=1.5)
+ax2_inlet.plot(Data['m**2'],Data['Residue'],'*')
+ax2_inlet.set_xlabel(r"m$^2$")
+ax2_inlet.set_ylabel("Leif")
+ax2_inlet.set_ylim([-0.00003,0.00003])
+ax2_inlet.ticklabel_format(style='sci',scilimits=(0,-7))
+ax2_inlet.axhline(y=0)
+plt.savefig("KEN008_XRR.png",format="png",dpi=300,bbox_inches='tight')#Saving file
 plt.show()
 
 #Fourier Thickness
-
-Xray.FourierThickness(DataXRR)
+fig3, ax3 = Xray.FourierThickness(DataXRR, None, None)
+plt.show()
